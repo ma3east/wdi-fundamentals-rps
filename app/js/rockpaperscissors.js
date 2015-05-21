1246a9d0fd746ed26d4ad970532d0d67f1e80c8c
Andrea Mckenzie <ma3east@gmail.com>
2015-05-21 15:20:02 +0100

    Have got app to prompt 5 times but commiting changes made.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -2,10 +2,10 @@
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
 'use strict';
-
+// changed getInput() to display proper message.
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    var message = ("Please choose either 'rock', 'paper', or 'scissors'.")
+    return prompt(message);
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -37,6 +37,8 @@ function getComputerMove(move) {
 
 function getWinner(playerMove,computerMove) {
     var winner;
+    playerMove = getPlayerMove();
+    computerMove = getComputerMove();
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
@@ -68,12 +70,64 @@ function getWinner(playerMove,computerMove) {
 }
     
  
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+/*function playToFive() {
+    /*console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+     //YOUR CODE HERE 
+    return [playerWins, computerWins];   
+    
+}*/
+// below is what i have done, havent managed to get it to prompt til 5, will display 5 results but for same first prompt
+
+function playToFive(){
+  var round = 0;
+  var playerWins = 0, computerWins = 0, tie = 0;
+  var result = getWinner();
+  var total;
+  console.log('Let\'s play Rock Paper Scissors');
+  
+    while((playerWins !== 5) || (computerWins !== 5)){
+    
+    
+        if(result){
+         
+          round += 1;  console.log("This is round: " + round);
+          
+          if(result === 'player'){
+          console.log("The winner of this round is: player");
+          playerWins++; 
+          }
+    
+          else if(result === 'computer'){
+          console.log("The winner of this round is: computer");
+          computerWins++; 
+          }
+          
+          else if(result === "tie"){
+          console.log("The result was a tie");
+          tie++;
+          }
+          
+          total = [playerWins, computerWins, tie];
+          
+        } 
+        console.log(("Scores: Player: " + playerWins + " Computer: " + computerWins + " Tied: " + tie)); 
+  
+        console.log(total);
+    
+        if((playerWins == 5) || (computerWins == 5)){
+            console.log("Good game!");
+            break;
+        
+        }else if(tie == 5){
+      
+          break;
+        }
+    
+  }
+   
 }
+playToFive();
 
